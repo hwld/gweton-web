08f120c2b7db21d36ce5e8171669290699c2eac7
hwld <0701hw@gmail.com>
2019-12-27 19:02:39 +0900

    検索の新しい実装

--- src/components/organisms/GwnSearchResultMemoList.vue
+++ src/components/organisms/GwnSearchResultMemoList.vue
@@ -3,7 +3,7 @@
     <v-card v-if="searchResultMemos[0] == null">
       <v-card-text>メモが見つかりませんでした</v-card-text>
     </v-card>
-    <GwnMemoListBase v-else :searchText="filterText" :memos="searchResultMemos"></GwnMemoListBase>
+    <GwnMemoListBase v-else :memos="searchResultMemos"></GwnMemoListBase>
     <v-footer padless fixed>
       <v-card width="100%" flat tile class="text-center">
         <v-card-text>
@@ -34,38 +34,37 @@ export default {
   },
 
   computed: {
-    genres() {
-      return this.$store.getters.getGenres;
-    },
-
-    selectedGenre() {
-      return this.$store.getters.getSelectedGenre;
-    },
-
-    filterText() {
-      return this.$store.getters.getFilterText;
-    },
-
     getGenreList() {
       return this.$store.getters.getGenreList;
     },
 
+    //検索対象のジャンルid
     searchTargetGenresId() {
       let searchTargetGenresId = [];
 
-      //ジャンルが選択されてなければすべてのジャンルIDを対象にする
-      if (this.selectedGenre.id == null) {
+      if (this.searchCriteria.isSelectAllGenre) {
+        //すべてのジャンルを対象に
+
         this.getGenreList.forEach(genre => {
           searchTargetGenresId.push(genre.id);
         });
       } else {
-        searchTargetGenresId.push(this.selectedGenre.id);
-        this.setSearchTargetGenresId(this.selectedGenre, searchTargetGenresId);
+        //選択されたジャンルを対象に
+        searchTargetGenresId.push(this.searchCriteria.genreId);
+
+        //選択されたジャンルに含まれるすべてのジャンルも対象に
+        if (!this.searchCriteria.isSelectGenreOnly) {
+          this.setSearchTargetGenresId(
+            this.getGenreById(this.searchCriteria.genreId),
+            searchTargetGenresId
+          );
+        }
       }
 
       return searchTargetGenresId;
     },
 
+    //検索対象のメモ
     searchTargetMemos() {
       let searchTargetMemos = [];
       this.searchTargetGenresId.forEach(genreId => {
@@ -75,27 +74,18 @@ export default {
       return searchTargetMemos;
     },
 
+    //検索条件に一致するメモ
     searchResultMemos() {
-      const text = this.filterText.toUpperCase();
-      if (text == "") return [];
-      return this.searchTargetMemos.filter(memo => {
-        if (
-          memo.title.toUpperCase().includes(text) ||
-          memo.text.toUpperCase().includes(text) ||
-          memo.authorName.toUpperCase().includes(text) ||
-          memo.bookName.toUpperCase().includes(text)
-        ) {
+      const hoge = this.searchTargetMemos.filter(memo => {
+        if (this.matchCriteria(memo)) {
           return memo;
         }
       });
+      return hoge;
     }
   },
 
   methods: {
-    getGenresByParentId(parentId) {
-      return this.$store.getters.getGenresByParentId(parentId);
-    },
-
     getGenreById(genreId) {
       return this.$store.getters.getGenreById(genreId);
     },
@@ -111,6 +101,34 @@ export default {
         let childGenre = this.getGenreById(childGenreId);
         this.setSearchTargetGenresId(childGenre, searchTargetGenresId);
       }
+    },
+
+    matchCriteria(memo) {
+      const cAuthorName = this.searchCriteria.authorName;
+      const cBookName = this.searchCriteria.bookName;
+
+      const cTitle = this.searchCriteria.title;
+      const memoTitle = memo.title;
+
+      const cText = this.searchCriteria.text;
+      const memoText = memo.text;
+
+      const bool =
+        (!cAuthorName || memo.authorName === cAuthorName) &&
+        (!cBookName || memo.bookName === cBookName) &&
+        (!cTitle || memoTitle.toUpperCase().includes(cTitle.toUpperCase())) &&
+        (!cText || memoText.toUpperCase().includes(cText.toUpperCase()));
+
+      window.console.log(memo);
+      window.console.log(!cAuthorName || memo.authorName === cAuthorName);
+      window.console.log(!cBookName || memo.authorName === cBookName);
+      window.console.log(
+        !cTitle || memoTitle.toUpperCase().includes(cTitle.toUpperCase())
+      );
+      window.console.log(
+        !cText || memoText.toUpperCase().includes(cText.toUpperCase())
+      );
+      return bool;
     }
   }
 };
