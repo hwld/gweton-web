94947d089bf0b2892b0aeaba73635da803f933f6
hwld <0701hw@gmail.com>
2019-12-28 10:06:55 +0900

    検索フィールドを削除

--- src/components/organisms/GwnHeader.vue
+++ src/components/organisms/GwnHeader.vue
@@ -3,21 +3,17 @@
     <slot name="prepend"></slot>
 
     <v-spacer></v-spacer>
-    <GwnSearchField></GwnSearchField>
-    <v-spacer></v-spacer>
-
     <GwnAuthState></GwnAuthState>
   </v-app-bar>
 </template>
 
 <script>
 import GwnAuthState from "@/components/organisms/GwnAuthState.vue";
-import GwnSearchField from "@/components/organisms/GwnSearchField.vue";
 
 export default {
   name: "GwnHeader",
 
-  components: { GwnAuthState, GwnSearchField }
+  components: { GwnAuthState }
 };
 </script>
 
